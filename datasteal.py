8295e6debd43ba31ba00e31a065fc9a9b7cfaf43
sigridveronica <47123213+sigridveronica@users.noreply.github.com>
2024-02-23 09:14:42 +0100

    Update datasteal.py

--- datasteal.py
+++ datasteal.py
@@ -1,3 +1,6 @@
+YOUR_SPREADSHEET_URL= 'https://docs.google.com/spreadsheets/d/1rZHxh0yrma_r6HTWMx0m3RFVyicsW5ei52xaNZ-RsyQ/edit?usp=sharing'
+
+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -11,16 +14,15 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', sco
 client = gspread.authorize(creds)
 
 # Open the spreadsheet
-spreadsheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1rZHxh0yrma_r6HTWMx0m3RFVyicsW5ei52xaNZ-RsyQ/edit?usp=sharing')
+spreadsheet = client.open_by_url('YOUR_SPREADSHEET_URL')
 worksheet = spreadsheet.get_worksheet(0)  # Assuming the data is in the first sheet
 
 # Open the database file to append the text
 with open('database.md', 'a') as db_file:
     # Iterate over each row in the worksheet
-    for row in worksheet.get_all_records():
+    for i, row in enumerate(worksheet.get_all_records(), start=2):  # Start from row 2
         if row['Generated text'] != '':
             # Write the text to the database file
             db_file.write(f"{row['Topic']},{row['Length']},{row['Style']},{row['Generated text']}\n")
             # Erase the text in the spreadsheet
-            worksheet.update_cell(row['Index'], 4, '')
-
+            worksheet.update_cell(i, 4, '')
